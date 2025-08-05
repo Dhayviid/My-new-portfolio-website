8b72f0de453f8f3d24d34d8cbf411cbad54e83c7
David Donald <phoenixdkd2004@gmail.com>
2025-08-05 10:23:17 +0100

    Added custom styling for meteors and added keyframes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 export const Home = () => {
   return (
-    <div className="min-h-screen">
+    <div className="min-h-screen  ">
       {/*Theme Toggle */}
       {/*Background Effects */}
       {/*NavBar */}
